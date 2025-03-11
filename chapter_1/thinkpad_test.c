e1a5d6a7f269bce80b63865318fcdbddec689b68
VesperVanity <192909324+VesperVanity@users.noreply.github.com>
2025-03-11 12:18:51 -0400

    ThinkPad test

---
+++ chapter_1/thinkpad_test.c
@@ -0,0 +1,9 @@
+/*
+The C Programming Language by Brian W. Kernighan and Dennis M. Ritchie
+Chapter 1 - Exercise 0
+
+*/
+//////////////////////////////////////////////////////////////////////////////////
+//Date: 03/11/25
+//Author: VV - VesperVanity - https://github.com/VesperVanity
+
