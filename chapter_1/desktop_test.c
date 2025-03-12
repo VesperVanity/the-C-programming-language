2206854c836bd1b04c370da42815c754d04cb2cf
VesperVanity <OCEAN.Jung@protonmail.com>
2025-03-12 14:48:22 -0400

    Desktop init test

---
+++ chapter_1/desktop_test.c
@@ -0,0 +1,15 @@
+/*
+The C Programming Language by Brian W. Kernighan and Dennis M. Ritchie
+Chapter 1 - Exercise 0
+
+*/
+//////////////////////////////////////////////////////////////////////////////////
+//Date: 03/12/25
+//Author: VV - VesperVanity - https://github.com/VesperVanity
+
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
