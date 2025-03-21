871c53090da5d5f8e54e8db15b602ec7ab821752
VesperVanity <192909324+VesperVanity@users.noreply.github.com>
2025-03-21 12:47:33 -0400

    sys reset and fully deleted

--- chapter_1/thinkpad_test.c
+++ chapter_1/thinkpad_test.c
@@ -7,3 +7,10 @@ Chapter 1 - Exercise 0
 //Date: 03/11/25
 //Author: VV - VesperVanity - https://github.com/VesperVanity
 
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "sys is reset fully and deleted.");
+	return 0;
+}
